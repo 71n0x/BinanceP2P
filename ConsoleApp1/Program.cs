93a6e38a944cd360d1b79d0271d0762564f2f092
StefanBulka <19156094+StefanBulka@users.noreply.github.com>
2022-01-22 18:03:22 +0100

    a

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace ConsoleApp1 // Note: actual namespace depends on the project name.
+﻿using PuppeteerExtraSharp;
+using PuppeteerExtraSharp.Plugins.ExtraStealth;
+using PuppeteerSharp;
+
+namespace ConsoleApp1 // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+            var extra = new PuppeteerExtra();
+            extra.Use(new StealthPlugin());
+
+            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+            var browser = await extra.LaunchAsync(new LaunchOptions
+            {
+                Headless = false,
+                Args = new[]
+                {
+                    "--no-sandbox", "--disable-setuid-sandbox", "--disable-infobars", "--window-position=0,0",
+                    "--ignore-certifcate-errors", "--ignore-certifcate-errors-spki-list",
+                   // $"--disable-extensions-except={Directory.GetCurrentDirectory()}/UblockOrigin"
+                },
+                UserDataDir = "userData"
+            });
         }
     }
 }
