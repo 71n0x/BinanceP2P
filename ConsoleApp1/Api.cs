e48106265a3ed608384ed97e57103ab4ed80a1b7
StefanBulka <19156094+StefanBulka@users.noreply.github.com>
2022-01-22 17:58:01 +0100

    a

---
+++ ConsoleApp1/Api.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using BinanceBestExchange;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1;
+
+public class Api
+{
+    public static HttpClient Hclient = new HttpClient();
+
+    public static async Task<RevolutApiResponse> GetRevolutExchangeRate(string from, string to)
+    {
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri($"https://www.revolut.com/api/exchange/quote?amount=100000&country=PL&fromCurrency={from}&isRecipientAmount=false&toCurrency={to}"),
+            //Content = new StringContent($"{{\"page\":1,\"rows\":20,\"payTypes\":[],\"asset\":\"USDT\",\"tradeType\":\"SELL\",\"fiat\":\"{currency}\",\"publisherType\":null,\"merchantCheck\":false}}",
+//Encoding.UTF8, "application/json"),
+        };
+        
+        request.Headers.Add("Accept-Language", "en");
+        
+        var retur = await Hclient.SendAsync(request);
+        var a = await retur.Content.ReadAsStringAsync();
+
+        a = a.Replace($",\"fxWeekend\":{{\"amount\":{{\"amount\":{{\"amount\":1000,\"currency\":\"{from}\"}},\"percentage\":\"1%\"}}}}", "");
+        
+        // var response = await Hclient.GetStringAsync(
+        //     $"https://www.revolut.com/api/exchange/quote?amount=100000&country=PL&fromCurrency={from}&isRecipientAmount=false&toCurrency={to}");
+        
+        
+        
+        return JsonConvert.DeserializeObject<RevolutApiResponse>(a); 
+    }
+
+    public static async Task<BinanceApiResponse> GetBinanceExchangeList(string currency, string asset, string tradeType)
+    {
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Post,
+            RequestUri = new Uri($"https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search"),
+            Content = new StringContent($"{{\"page\":1,\"rows\":5,\"payTypes\":[\"REVOLUT\"],\"asset\":\"{asset}\",\"tradeType\":\"{tradeType}\",\"fiat\":\"{currency}\",\"publisherType\":null,\"merchantCheck\":false}}",
+                Encoding.UTF8, "application/json"),
+        };
+        
+        var retur = await Hclient.SendAsync(request);
+        var a = await retur.Content.ReadAsStringAsync();
+
+        return JsonConvert.DeserializeObject<BinanceApiResponse>(a);
+    }
+}
---
+++ ConsoleApp1/RevolutApiResponse.cs
@@ -0,0 +1,78 @@
+﻿namespace BinanceBestExchange;
+
+// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+public class Sender
+{
+    public int amount { get; set; }
+    public string currency { get; set; }
+}
+
+public class Recipient
+{
+    public int amount { get; set; }
+    public string currency { get; set; }
+}
+
+public class Rate
+{
+    public string from { get; set; }
+    public string to { get; set; }
+    public double rate { get; set; }
+    public long timestamp { get; set; }
+}
+
+public class Fx
+{
+    public int amount { get; set; }
+    public string currency { get; set; }
+}
+
+public class Amount2
+{
+    public int amount { get; set; }
+    public string currency { get; set; }
+    public string percentage { get; set; }
+}
+
+public class FxWeekend
+{
+    public double amount { get; set; }
+}
+
+public class Total
+{
+    public int amount { get; set; }
+    public string currency { get; set; }
+}
+
+public class Cost
+{
+    public int amount { get; set; }
+    public string currency { get; set; }
+}
+
+public class Fees
+{
+    public Fx fx { get; set; }
+    public FxWeekend fxWeekend { get; set; }
+    public Total total { get; set; }
+    public Cost cost { get; set; }
+}
+
+public class Plan
+{
+    public string id { get; set; }
+    public string name { get; set; }
+    public Fees fees { get; set; }
+    public string tooltipLong { get; set; }
+    public string tooltipShort { get; set; }
+}
+
+public class RevolutApiResponse
+{
+    public Sender sender { get; set; }
+    public Recipient recipient { get; set; }
+    public Rate rate { get; set; }
+    public string fxTooltip { get; set; }
+    public List<Plan> plans { get; set; }
+}
